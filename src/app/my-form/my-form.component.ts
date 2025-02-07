b5c03877a03a26a5f80980b845bdfa1c673955a4
4N70N1017 <4n70n1017@gmail.com>
2025-02-07 10:48:21 -0600

    Agregacion de botones para los nuevos metodos continuar , pausar , y reiniciar

--- src/app/my-form/my-form.component.ts
+++ src/app/my-form/my-form.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
 
 @Component({
   selector: 'app-my-form',
   templateUrl: './my-form.component.html',
-  styleUrls: ['./my-form.component.css']
+  styleUrls: ['./my-form.component.css'],
+  imports: [CommonModule], // Asegúrate de incluir CommonModule en los imports
 })
 export class MyFormComponent implements OnInit {
   message: string = '';
-  iteration1: { iteration: number, threadId: string, pid: number, progress: number } = { iteration: 0, threadId: '', pid: 0, progress: 0 };
-  iteration2: { iteration: number, threadId: string, pid: number, progress: number } = { iteration: 0, threadId: '', pid: 0, progress: 0 };
-  iteration3: { iteration: number, threadId: string, pid: number, progress: number } = { iteration: 0, threadId: '', pid: 0, progress: 0 };
+  iteration1: {
+    iteration: number;
+    threadId: string;
+    pid: number;
+    progress: number;
+  } = { iteration: 0, threadId: '', pid: 0, progress: 0 };
+  iteration2: {
+    iteration: number;
+    threadId: string;
+    pid: number;
+    progress: number;
+  } = { iteration: 0, threadId: '', pid: 0, progress: 0 };
+  iteration3: {
+    iteration: number;
+    threadId: string;
+    pid: number;
+    progress: number;
+  } = { iteration: 0, threadId: '', pid: 0, progress: 0 };
   pids: number[] = [];
   readonly maxIterations = 10000000; // Máximo de iteraciones
 
@@ -21,25 +38,34 @@ export class MyFormComponent implements OnInit {
   }
 
   startLoops(): void {
-    this.dataService.startLoops().subscribe(response => {
+    this.dataService.startLoops().subscribe((response) => {
       this.message = response.message;
     });
   }
 
   getIteration(loopNumber: number): void {
-    this.dataService.getIteration(loopNumber).subscribe(response => {
+    this.dataService.getIteration(loopNumber).subscribe((response) => {
       if (loopNumber === 1) {
-        this.iteration1 = { ...response, progress: this.calculateProgress(response.iteration) };
+        this.iteration1 = {
+          ...response,
+          progress: this.calculateProgress(response.iteration),
+        };
       } else if (loopNumber === 2) {
-        this.iteration2 = { ...response, progress: this.calculateProgress(response.iteration) };
+        this.iteration2 = {
+          ...response,
+          progress: this.calculateProgress(response.iteration),
+        };
       } else if (loopNumber === 3) {
-        this.iteration3 = { ...response, progress: this.calculateProgress(response.iteration) };
+        this.iteration3 = {
+          ...response,
+          progress: this.calculateProgress(response.iteration),
+        };
       }
     });
   }
 
   getPIDs(): void {
-    this.dataService.getPIDs().subscribe(response => {
+    this.dataService.getPIDs().subscribe((response) => {
       this.pids = response;
     });
   }
